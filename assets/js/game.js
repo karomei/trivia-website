555db224c2f40ea3a1740b7f8827842163c91ddb
58730948 <58730948+karomei@users.noreply.github.com>
2020-03-25 16:28:44 +0100

    Hard-code questions

---
+++ assets/js/game.js
@@ -0,0 +1,81 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswer = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+ {
+     question: "Who is the cutest?",
+     choice1: "My mama",
+     choice2: "A kitten",
+     choice3: "Leonardo DiCaprio",
+     choice4: "Christian",
+     answer: 4
+ },
+ {
+    question: "Who shot the sheriff?",
+    choice1: "Bob Marley",
+    choice2: "You",
+    choice3: "Me",
+    choice4: "Jeanie with the gun",
+    answer: 1
+ },
+ {
+    question: "What is the meaning of life?",
+    choice1: "Love",
+    choice2: "42",
+    choice3: "43",
+    choice4: "There's none",
+    answer: 2
+ }
+];
+
+//-------------------------------------CONSTANTS
+
+const CORRECT_BONUS = 5;
+const MAX_QUESTIONS = 3;
+
+startGame = () =>{
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("./finish.html");
+    };
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswer = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e =>{
+        if(!acceptingAnswer) return;
+
+        acceptingAnswer = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        
+        getNewQuestion();
+    });
+});
+
+startGame();
